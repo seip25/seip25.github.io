9163039886a6ee0b3c217908ede61692bbd69720
Andres Paiva <andrespaivasilveiraa@gmail.com>
2025-07-11 10:27:48 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -28,23 +28,23 @@ window.addEventListener('load', () => {
 });
 
 
-// window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-//     theme(window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
-// })
-// document.addEventListener('DOMContentLoaded', () => {
-//     theme();
-// });
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+    theme(window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
+})
+document.addEventListener('DOMContentLoaded', () => {
+    theme();
+});
 
-// function theme(theme_ = false) {
-//     let theme = theme_ ? theme_ : window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+function theme(theme_ = false) {
+    let theme = theme_ ? theme_ : window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 
-//     if (theme_) {
-//         localStorage.setItem('theme', theme_);
-//     }
-//     if (localStorage.getItem('theme') === 'dark' || localStorage.getItem('theme') === 'light') {
-//         theme = localStorage.getItem('theme');
-//     }
-//     document.documentElement.setAttribute('data-theme', theme)
+    if (theme_) {
+        localStorage.setItem('theme', theme_);
+    }
+    if (localStorage.getItem('theme') === 'dark' || localStorage.getItem('theme') === 'light') {
+        theme = localStorage.getItem('theme');
+    }
+    document.documentElement.setAttribute('data-theme', theme)
 
-//     return theme;
-// }
+    return theme;
+}
